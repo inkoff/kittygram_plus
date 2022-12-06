ea79af5b8c387d6e0d74d4b66a35c56bf072b1ce
inkoff <kob87@list.ru>
2022-12-06 11:24:21 +0300

    viewset

--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,45 @@
+from rest_framework import mixins
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from .models import Cat
-from .serializers import CatSerializer
+from cats.models import Cat, Owner
+from cats.serializers import CatSerializer, OwnerSerializer, CatListSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+    @action(detail=False, url_path="recent-white-cats")
+    def recent_white_cats(self, request):
+        cats = Cat.objects.filter(color__exact="White")[:5]
+        serializer: CatSerializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return CatListSerializer
+        return CatSerializer
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+
+
+class UpdateDeleteViewSet(
+    mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet
+):
+    pass
+
+
+class CreateRetrieveViewSet(
+    mixins.CreateModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet
+):
+    pass
+
+
+class LightCatViewSet(CreateRetrieveViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,13 +1,13 @@
 from django.urls import include, path
-
 from rest_framework.routers import DefaultRouter
 
-from cats.views import CatViewSet
-
+from cats.views import CatViewSet, OwnerViewSet, LightCatViewSet
 
 router = DefaultRouter()
-router.register('cats', CatViewSet)
+router.register("cats", CatViewSet)
+router.register("owners", OwnerViewSet)
+router.register(r"my cats", LightCatViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
